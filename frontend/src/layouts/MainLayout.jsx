f41ae1be3d79ecc306733ef66f6d2084f259e3e4
Yusuf AL <yusufal5558@gmail.com>
2024-08-13 00:42:37 +0300

    Dialog modal

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -3,12 +3,15 @@ import Header from "../components/Layout/Header/Header"
 import PropTypes from "prop-types"
 import Search from "../components/Modals/Search/Search"
 import { useState } from "react"
+import Dialog from "../components/Modals/Dialog/Dialog"
 
 const MainLayout = ({children}) => {
   const [isSearchShow, setIsSearchShow] = useState(false)
+  const [isDialogShow, setIsDialogShow] = useState(true)
 
   return (
     <div className="main-layout">
+      <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow}/>
       <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow}/>
         <Header setIsSearchShow={setIsSearchShow}/>
         {children}
